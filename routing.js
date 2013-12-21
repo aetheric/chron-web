aca1364c98ae765fda324260eae493c61e260caf
Peter Cummuskey <peterc@aetheric.co.nz>
2013-12-22 10:06:37 +1300

    Extracting detail page routing to separate file

--- routing.js
+++ routing.js
@@ -59,48 +59,7 @@ module.exports = function(express) {
 	});
 	
 	// Detail page rendering
-	express.get('/detail/:detailId?/:detailSlug?', function(req, res) {
-
-		var requestId = req.params.detailId;
-		if (!requestId) {
-			res.render('detail_missing', {
-				itemId: null,
-				title: 'Details Missing'
-			});
-
-			return;
-		}
-
-		// Retrieve detail item by req.params.detailId.
-		var detailItem = ( requestId == 5 ? {
-			id: req.params.detailId,
-			slug: 'other-slug',
-			title: 'Test Detail Item'
-		} : null );
-
-		if (!detailItem) {
-			res.render('detail_missing', {
-				itemId: requestId,
-				title: 'Details Missing',
-				script: null
-			});
-
-			return;
-		};
-		
-		// if detail slug is missing / incorrect, permanent redirect to correct slug.
-		var requestSlug = req.params.detailSlug;
-		if (!requestSlug || requestSlug !== detailItem.slug) {
-			res.redirect(301, detailLink(detailItem.id, detailItem.slug));
-			return;
-		}
-
-		res.render('detail', {
-			item: detailItem,
-			title: detailItem.title,
-			script: null
-		});
-	});
+	require('./routing_detail')(express);
 
 	// Set up generic view renderer.
 	express.get('/view/:viewName', function(req, res) {
---
+++ routing_detail.js
@@ -0,0 +1,46 @@
+module.exports = function(express) {
+
+	express.get('/detail/:detailId?/:detailSlug?', function(req, res) {
+
+		var requestId = req.params.detailId;
+		if (!requestId) {
+			res.render('detail_missing', {
+				itemId: null,
+				title: 'Details Missing'
+			});
+
+			return;
+		}
+
+		// Retrieve detail item by req.params.detailId.
+		var detailItem = ( requestId == 5 ? {
+			id: req.params.detailId,
+			slug: 'other-slug',
+			title: 'Test Detail Item'
+		} : null );
+
+		if (!detailItem) {
+			res.render('detail_missing', {
+				itemId: requestId,
+				title: 'Details Missing',
+				script: null
+			});
+
+			return;
+		};
+		
+		// if detail slug is missing / incorrect, permanent redirect to correct slug.
+		var requestSlug = req.params.detailSlug;
+		if (!requestSlug || requestSlug !== detailItem.slug) {
+			res.redirect(301, detailLink(detailItem.id, detailItem.slug));
+			return;
+		}
+
+		res.render('detail', {
+			item: detailItem,
+			title: detailItem.title,
+			script: null
+		});
+	});
+
+};

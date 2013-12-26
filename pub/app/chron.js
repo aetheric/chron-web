15a6614cbadf9ff7b2a060aaaac74d3277bd10d6
Peter Cummuskey <peterc@aetheric.co.nz>
2013-12-26 19:45:14 +1300

    Added angular-websocket to module dependencies. Ref #30.

--- pub/app/chron.js
+++ pub/app/chron.js
@@ -3,10 +3,12 @@ define([
 	'angular_ui',
 	'angular_route',
 	'angular_mocks',
+	'angular_websocket'
 ], function(angular) {
 
 	return angular.module('chron', [
 		'ngRoute',
+		'angular-websocket',
 		'ngMockE2E'
 	]);
 

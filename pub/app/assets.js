c24a3f9b3f479d72428ab04b7b820469b250a8c0
tzrlk <peterc@aetheric.co.nz>
2013-12-28 15:40:00 +1300

    Adding in missing punctuation, and remote link for angular_websocket. Closes #32.

--- pub/app/assets.js
+++ pub/app/assets.js
@@ -34,8 +34,9 @@ require.config({
 		],
 
 		angular_websocket: [
+			'//raw.github.com/gdi2290/angular-websocket/master/angular-websocket',
 			'lib/angular-websocket'
-		]
+		],
 
 		text: [
 			'//cdnjs.cloudflare.com/ajax/libs/require-text/2.0.10/text',
@@ -76,7 +77,7 @@ require.config({
 			init: function(angular) {
 				return angular.module('angular-websocket')
 			}
-		}
+		},
 
 		underscore: {
 			exports: '_'
--- routing_login.js
+++ routing_login.js
@@ -33,4 +33,4 @@ module.exports = function(express) {
 		res.redirect('/app');
 	});
 
-}
+};

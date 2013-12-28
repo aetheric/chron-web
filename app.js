0b1ea7a9e55fabb21ac281c5defa34e8e7f58088
tzrlk <peterc@aetheric.co.nz>
2013-12-28 17:43:00 +1300

    Making sure repl only runs in dev. Ref #36.

--- app.js
+++ app.js
@@ -12,14 +12,22 @@ require('./routing')(express);
 
 // Start the server
 console.log("Express starting on port ", port);
-var server = require('http')
-	.createServer(express)
-	.listen(port);
 
-// Start a repl for easy eval/shutdown
-require('repl').start({
-	input: process.stdin,
-	output: process.stdout
-}).on('exit', function() {
-	server.close();
-});
+if ('prod'.indexOf(process.env.NODE_ENV) < 0) {
+
+	var server = require('http')
+		.createServer(express)
+		.listen(port);
+
+	// Start a repl for easy eval/shutdown
+	require('repl').start({
+		input: process.stdin,
+		output: process.stdout
+	}).on('exit', function () {
+		server.close();
+	});
+
+} else {
+	// When running on production...
+	express.listen(port);
+}
